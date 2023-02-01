61de7f64951ae4b900df5ca26464143279618275
Julian Pabloff <julianpabloff@gmail.com>
2023-02-01 04:23:19 -0800

    Added another section, more animation refinements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', function() {
+	let scrollY = window.scrollY;
+
+	///// MOTTO /////
 	const mottoContainer = document.getElementById('motto-container');
 	const mottoChildren = [];
 	for (const child of mottoContainer.children) mottoChildren.push(child);
@@ -19,7 +22,8 @@ window.addEventListener('load', function() {
 	}
 
 	function handleMottoDisplacement() {
-		const availableDisplacement = 1100 - mottoTop - mottoContainer.clientHeight;
+		const fadeEarlyOffset = 200 * !mobile;
+		const availableDisplacement = 1100 - mottoTop - mottoContainer.clientHeight - fadeEarlyOffset;
 		const displacement = availableDisplacement / landingImageHalfwayPointScrollY * scrollY; // 1100 = Height of the landing image; 400 = available displacement before motto leaks out of image
 		// Y = X(400 / threshold); X = scrollY;
 		mottoContainer.style.transform = 'translateY(' + displacement + 'px)';
@@ -55,12 +59,9 @@ window.addEventListener('load', function() {
 		}
 	}
 
-	let scrollY = window.scrollY;
-
 	function setSize() {
 		windowWidth = window.innerWidth;
 		windowHeight = window.innerHeight;
-		landingImageHalfwayPointScrollY = 1100 - windowHeight / 2; // The scrollY point where the landing image is halfway up the screen - when the motto text should fade away
 		mottoTop = mottoContainer.offsetTop;
 		currentMottoDisplacement = mottoTop;
 	}
@@ -70,14 +71,21 @@ window.addEventListener('load', function() {
 
 	let mobile = !(window.innerWidth > 767);
 	const elevatorSection = document.getElementById('elevator-section');
+	// const whyWoolleyTextContainer = document.getElementById('why-whoolley-text-container');
+	// console.log(whyWoolleyTextContainer);
 	function handleResize() {
 		windowHeight = window.innerHeight;
 		windowWidth = window.innerWidth;
+		landingImageHalfwayPointScrollY = 900 - windowHeight / 2; // The scrollY point where the landing image is halfway up the screen - when the motto text should fade away
 		if (windowWidth <= 1024) {
 			const displacement = windowWidth - 1024;
 			elevatorSection.style['background-position-x'] = displacement + 'px';
+			// const centerX = windowWidth / 2 - whyWoolleyTextContainer.clientWidth / 2;
+			// whyWoolleyTextContainer.style.left = centerX.toString() + 'px';
 		} else {
 			elevatorSection.style['background-position-x'] = 'initial';
+			// whyWoolleyTextContainer.style.transition = 'left 0.5s';
+			// whyWoolleyTextContainer.style.left = '0';
 		}
 		if (windowWidth > 767) {
 			if (mobile) {
@@ -95,44 +103,78 @@ window.addEventListener('load', function() {
 	handleMottoDisplacement();
 
 
+	class scrollFadeInElement {
+		constructor(element, offset = 0) {
+			this.element = element;
+			this.top = element.offsetTop;
+			this.height = element.clientHeight;
+			this.offset = offset;
+		}
+		handleOpacity(scrollY) {
+			const beginning = this.top - windowHeight + this.offset;
+			const halfway = this.top + this.height / 2 - windowHeight / 2;
+			const opacity = (scrollY - beginning) / (halfway - beginning);
+			this.element.style.opacity = opacity > 0 ? opacity.toString() : '0';
+		}
+		handleResize() {
+			this.top = this.element.offsetTop;
+			this.height = this.element.clientHeight;
+		}
+		update() {
+			this.handleResize();
+			this.handleOpacity(window.scrollY);
+		}
+	}
+
+
 	///// CATEGORIES /////
 	const categoryContainer = document.getElementById('category-container');
 	console.log(categoryContainer);
-	const categories = [];
-	for (const child of categoryContainer.children) categories.push(child);
-	const categoryTop = categoryContainer.offsetTop;
-	const categoryHeight = categories[0].clientHeight;
-	console.log(categoryContainer);
-	console.log(categories);
-	console.log(categoryTop);
-	const categoryBeginningScroll = categoryTop - windowHeight;
-	const categoryHalfScroll = categoryTop + categoryHeight / 2 - windowHeight / 2;
 
-	function handleCategoryOpacity() {
+	function updateCategoryFadeIn(scrollFade, index) {
+		scrollFade.handleResize();
+		scrollFade.handleOpacity(scrollY);
+		scrollFade.offset = 50 + 90 * index * (windowWidth >= 767);
+	}
+	function updateCategoryFadeIns() {
 		let i = 0;
-		for (const child of categories) {
-			const mobile = windowWidth < 767;
-			const offset = 100 + 90 * i;
-			const top = categoryTop + categoryHeight * i * mobile;
-			const beginning = top - windowHeight + offset * !mobile;
-			const halfway = top + categoryHeight / 2 - windowHeight / 2;
-			const opacity = (scrollY - beginning) / (halfway - beginning);
-			child.style.opacity = opacity > 0 ? opacity.toString() : '0';
+		for (const scrollFade of categoryScrollFades) {
+			updateCategoryFadeIn(scrollFade, i);
 			i++;
 		}
 	}
+
+	const categoryScrollFades = [];
+	const categoryChildrenLength = categoryContainer.children.length;
+	for (let i = 0; i < categoryChildrenLength; i++) {
+		const child = categoryContainer.children[i];
+		const categoryFadeIn = new scrollFadeInElement(child);
+		updateCategoryFadeIn(categoryFadeIn, i);
+		categoryScrollFades.push(categoryFadeIn);
+	}
+
+
+	///// WHY WHOOLLEY /////
+	const whyWhoolleyHeader = document.getElementById('why-whoolley-header');
+	const whyWhoolleyTop = whyWhoolleyHeader.offsetTop;
+	const whyWhoolleyHeight = whyWhoolleyTop.clientHeight;
+	console.log(whyWhoolleyHeader);
+
+	const whyWhoolleyHeaderFadeIn = new scrollFadeInElement(whyWhoolleyHeader, 50);
 
 	document.addEventListener('scroll', event => {
 		scrollY = window.scrollY;
 		handleMottoDisplacement(); // must be called first because its displacement affects when the motto gets revealed
 		if (mottoChildrenRevealed < 7) handleMottoReveal(); // 1 1 1 => 2 1 0 indeces of motto words
-
-		handleCategoryOpacity();
+		updateCategoryFadeIns();
+		whyWhoolleyHeaderFadeIn.update();
 	});
 
 	window.addEventListener('resize', event => {
 		handleResize();
 		handleMottoDisplacement();
+		updateCategoryFadeIns();
+		whyWhoolleyHeaderFadeIn.update();
 	});
 
 	setTimeout(() => mottoContainer.style.transition = 'transform 1s cubic-bezier(0, 0.33, 0.07, 1.03)', 200);
